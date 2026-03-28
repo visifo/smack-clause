b2723861cbd24339c968e406ca59d48291cb6af7
Sergej Tihonov <sergej@visifo.com>
2026-03-28 23:15:43 +0100

    add Arch test to enforce `Smackable` implementation in `Types` namespace

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 
 use Visifo\SmackClause\Smack;
+use Visifo\SmackClause\Smackable;
 
 arch()->preset()->php()->ignoring(Smack::class);
 
@@ -9,3 +10,7 @@ arch()->preset()->security();
 it('will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->not->toBeUsed();
+
+arch('implements Smackable')
+    ->expect('Visifo\SmackClause\Types')
+    ->toImplement(Smackable::class);
